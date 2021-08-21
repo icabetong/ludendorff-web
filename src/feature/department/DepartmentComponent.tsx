2b77380ad08bbd0e14aed3749b7ca0f6a070660b
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-08-21 22:10:11 +0800

    Refactored DepartmentList

--- src/feature/department/DepartmentComponent.tsx
+++ src/feature/department/DepartmentComponent.tsx
@@ -1,39 +1,14 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import IconButton from "@material-ui/core/IconButton";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import Typography from "@material-ui/core/Typography";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-
-import { TrashIcon, OfficeBuildingIcon } from "@heroicons/react/outline";
-
-import PaginationController from "../../components/PaginationController";
-import EmptyStateComponent from "../state/EmptyStates";
+import { useTheme } from "@material-ui/core/styles";
 
 import { Department } from "./Department";
-
-const useStyles = makeStyles(() => ({
-    root: {
-        minHeight: '60vh'
-    },
-    stateIcon: {
-        width: '4em',
-        height: '4em'
-    },
-    listItemIcon: {
-        width: '1em',
-        height: '1em',
-    }
-}));
+import DepartmentList from "./DepartmentList";
 
 type DepartmentComponentPropsType = {
     isOpen: boolean,
@@ -67,10 +42,10 @@ const DepartmentComponent = (props: DepartmentComponentPropsType) => {
                     departments={props.departments}
                     hasPrevious={props.hasPrevious}
                     hasNext={props.hasNext}
-                    onPreviousBatch={props.onPrevious}
-                    onNextBatch={props.onNext}
+                    onPrevious={props.onPrevious}
+                    onNext={props.onNext}
                     onItemSelect={props.onSelectItem}
-                    onItemDelete={props.onDeleteItem}/>
+                    onItemRemove={props.onDeleteItem}/>
             </DialogContent>
             <DialogActions>
             <Button color="primary" onClick={() => props.onAddItem()}>{ t("add") }</Button>
@@ -78,82 +53,6 @@ const DepartmentComponent = (props: DepartmentComponentPropsType) => {
                 <Button color="primary" onClick={() => props.onDismiss()}>{ t("close") }</Button>
             </DialogActions>
         </Dialog>
-    )
-}
-
-type DepartmentListPropsType = {
-    departments: Department[],
-    hasPrevious: boolean,
-    hasNext: boolean,
-    onPreviousBatch: () => void,
-    onNextBatch: () => void,
-    onItemSelect: (department: Department) => void,
-    onItemDelete: (department: Department) => void
-}
-
-const DepartmentList = (props: DepartmentListPropsType) => {
-    const classes = useStyles();
-    const { t } = useTranslation();
-
-    return (
-        <React.Fragment>
-            { props.departments.length > 0
-                ? <React.Fragment>
-                    <List className={classes.root}>{
-                        props.departments.map((department: Department) => {
-                            return <DepartmentItem
-                                        key={department.departmentId}
-                                        department={department}
-                                        onItemSelect={props.onItemSelect}
-                                        onItemDelete={props.onItemDelete}/>
-                        })
-                    }</List>
-                    { !props.hasNext && !props.hasPrevious &&
-                        <PaginationController
-                            hasPrevious={props.hasPrevious}
-                            hasNext={props.hasNext}
-                            getPrevious={props.onPreviousBatch}
-                            getNext={props.onNextBatch}/>
-                    }
-                    </React.Fragment>
-            :   <EmptyStateComponent
-                    icon={<OfficeBuildingIcon className={classes.stateIcon}/>}
-                    title={t("empty_department")}
-                    subtitle={t("empty_department_summary")}/>
-            } 
-        </React.Fragment>
-    )
-}
-
-type DepartmentItemPropsType = {
-    department: Department,
-    onItemSelect: (department: Department) => void,
-    onItemDelete: (department: Department) => void,
-}
-
-const DepartmentItem = (props: DepartmentItemPropsType) => {
-    const { t } = useTranslation();
-    const classes = useStyles();
-
-    return (
-        <ListItem
-            dense
-            button
-            key={props.department.departmentId}
-            onClick={() => props.onItemSelect(props.department)}>
-            <ListItemText
-                primary={
-                    <Typography variant="body2">{props.department.name}</Typography>
-                }
-                secondary={
-                    <Typography variant="caption">{props.department.managerSSN?.name}</Typography>
-                }/>
-            <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label={t("delete")} onClick={() => props.onItemDelete(props.department)}>
-                    <TrashIcon className={classes.listItemIcon}/>
-                </IconButton>
-            </ListItemSecondaryAction>
-        </ListItem>
     )
 }
 
--- src/feature/department/DepartmentList.tsx
+++ src/feature/department/DepartmentList.tsx
@@ -3,16 +3,16 @@ import { useTranslation } from "react-i18next";
 import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { TagIcon, TrashIcon } from "@heroicons/react/outline";
+import { OfficeBuildingIcon, TrashIcon } from "@heroicons/react/outline";
 
 import EmptyStateComponent from "../state/EmptyStates";
 import PaginationController from "../../components/PaginationController";
 
-import { Category } from "./Category";
+import { Department } from "./Department";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -30,33 +30,33 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-type CategoryListProps = {
-    categories: Category[],
+type DepartmentListProps = {
+    departments: Department[],
     hasPrevious: boolean,
     hasNext: boolean,
     onPrevious: () => void,
     onNext: () => void,
-    onItemSelect: (category: Category) => void,
-    onItemRemove: (category: Category) => void
+    onItemSelect: (department: Department) => void,
+    onItemRemove: (department: Department) => void
 }
 
-const CategoryList = (props: CategoryListProps) => {
+const DepartmentList = (props: DepartmentListProps) => {
     const classes = useStyles();
     const { t } = useTranslation();
 
     return (
         <React.Fragment>
-            { props.categories.length > 0 
+            { props.departments.length > 0
             ?   <React.Fragment>
                     <List className={classes.root}>{
-                        props.categories.map((category: Category) => {
+                        props.departments.map((department: Department) => {
                             return (
-                                <CategoryItem 
-                                        key={category.categoryId} 
-                                        category={category} 
-                                        onItemSelect={props.onItemSelect}
-                                        onItemRemove={props.onItemRemove}/>
-                            );
+                                <DepartmentItem
+                                    key={department.departmentId}
+                                    department={department}
+                                    onItemSelect={props.onItemSelect}
+                                    onItemRemove={props.onItemRemove}/>
+                            )
                         })
                     }</List>
                     { !props.hasNext && !props.hasPrevious &&
@@ -68,40 +68,39 @@ const CategoryList = (props: CategoryListProps) => {
                     }
                 </React.Fragment>
             : <EmptyStateComponent
-                icon={<TagIcon className={classes.uiIcon}/>}
-                title={t("empty_category")}
-                subtitle={t("empty_category_summary")}/>
-
-        }
+                icon={<OfficeBuildingIcon className={classes.uiIcon}/>}
+                title={t("empty_department")}
+                subtitle={t("empty_department_summary")}/>
+            }
         </React.Fragment>
     );
 }
 
-type CategoryItemProps = {
-    category: Category,
-    onItemSelect: (category: Category) => void,
-    onItemRemove: (category: Category) => void,
+type DepartmentItemProps = {
+    department: Department,
+    onItemSelect: (department: Department) => void,
+    onItemRemove: (department: Department) => void
 }
 
-const CategoryItem = (props: CategoryItemProps) => {
+const DepartmentItem = (props: DepartmentItemProps) => {
     const { t } = useTranslation();
     const classes = useStyles();
 
     return (
         <ListItem
             button
-            key={props.category.categoryId}
-            onClick={() => props.onItemSelect(props.category)}>
-            <ListItemText 
-                primary={props.category.categoryName}
-                secondary={ t("count", { count: props.category.count }) }/>
+            key={props.department.departmentId}
+            onClick={() => props.onItemSelect(props.department)}>
+            <ListItemText
+                primary={props.department.name}
+                secondary={props.department.managerSSN?.name}/>
             <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label={t("delete")} onClick={() => props.onItemRemove(props.category)}>
+                <IconButton edge="end" aria-label={t("delete")} onClick={() => props.onItemRemove(props.department)}>
                     <TrashIcon className={classes.actionIcon}/>
                 </IconButton>
             </ListItemSecondaryAction>
         </ListItem>
     )
-}
+} 
 
-export default CategoryList;
+export default DepartmentList;
